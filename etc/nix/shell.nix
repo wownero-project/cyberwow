517c9fb3d5f3dae502a09f33f6511b3b459bd738
fuwa <fuwa25519@protonmail.com>
2021-03-06 03:25:28 +0800

    use v0.8.3.5

--- etc/nix/shell.nix
+++ etc/nix/shell.nix
@@ -105,8 +105,8 @@ let
     ; }
 
 
-; lolnero-rev = "v0.8.3.4"
-; lolnero-sha256 = "18wj687zzjxpmwn36s322g2z70mrhr86jl86j9r26ikdlgvqyq33"
+; lolnero-rev = "v0.8.3.5"
+; lolnero-sha256 = "19rig3p0ja0dkcj2qdgcq3c7w38r5zis2llyb8vsd79y0xb4113d"
 
 ; lolnero-source =
     nixpkgs.fetchgit
